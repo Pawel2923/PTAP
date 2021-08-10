01c860f4ce1e34bb497f35c93d4fade6ceb87b31
Pawel2923 <83728661+Pawel2923@users.noreply.github.com>
2021-08-10 14:58:15 +0200

    Dodanie stronki

---
+++ main.js
@@ -0,0 +1,36 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    let navbarHeight = $("#navbar").height() + 30;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector("#navbar").style.top = "0";
+    }
+    else {
+        $("#navbar").css("top", -navbarHeight);
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+$(document).ready(function() {
+    if (prevScrollpos < $(window).height()) {
+        if (window.location.href.indexOf("helikoptery") > -1) {
+            const element = document.querySelector("#heli");
+            element.scrollIntoView();
+            $("#heli-link").addClass("option-selected");
+        }
+        else if (window.location.href.indexOf("kontakt") > -1) {
+            const element = document.querySelector("#contact");
+            element.scrollIntoView();
+            $("#contact-link").addClass("option-selected");
+        }
+    }
+});
+
+$(".option").on("click", function () {
+    $(".option").removeClass("option-selected");
+    $(this).attr("id") === "home-link" ? $(this).removeClass("option-selected") : $(this).addClass("option-selected");
+});
+
+$(".logo").click(function () {
+    location = "home";
+});
