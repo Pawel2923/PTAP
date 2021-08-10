79bff079d894f61de9f4e91adf93d8acac3f7c20
Pawel2923 <83728661+Pawel2923@users.noreply.github.com>
2021-08-10 15:19:42 +0200

    poprawki
    poprawki przy linkach

--- main.js
+++ main.js
@@ -16,19 +16,12 @@ $(document).ready(function() {
         if (window.location.href.indexOf("helikoptery") > -1 || window.location.href.indexOf('/#heli') > -1) {
             const element = document.querySelector("#heli");
             element.scrollIntoView();
-            $("#heli-link").addClass("option-selected");
         }
         else if (window.location.href.indexOf("kontakt") > -1 || window.location.href.indexOf('/#contact') > -1) {
             const element = document.querySelector("#contact");
             element.scrollIntoView();
-            $("#contact-link").addClass("option-selected");
         }
     }
-});
-
-$(".option").on("click", function () {
-    $(".option").removeClass("option-selected");
-    $(this).attr("id") === "home-link" ? $(this).removeClass("option-selected") : $(this).addClass("option-selected");
 });
 
 $(".logo").click(function () {
