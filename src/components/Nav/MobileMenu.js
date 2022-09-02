28f3ae9db36fa1527bc8dad81ce66eabb6feeec1
Paweł <pawelporemba123@gmail.com>
2022-09-02 12:49:31 +0200

    Change MobileMenu size Add Backdrop Add close handlers on links

--- src/components/Nav/MobileMenu.js
+++ src/components/Nav/MobileMenu.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import React, { useRef } from "react";
 import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
 
@@ -8,25 +8,46 @@ const targetContainer = document.getElementById("overlays");
 
 const MobileMenu = (props) => {
   const overlay = useRef();
+  const backdrop = useRef();
 
   const closeClickHandler = () => {
+    backdrop.current.style.animationName = classes.fadeOut;
     overlay.current.style.animationName = classes.slideOut;
 
     setTimeout(() => {
-        props.onClose();
+      props.onClose();
     }, 200);
   };
 
   const children = (
-    <div className={classes.overlay} ref={overlay}>
-      <i className="fa-solid fa-xmark" onClick={closeClickHandler}></i>
-      <ul>
-        <li><Link to="/">HOME</Link></li>
-        <li><Link to="/wiki">WIKI</Link></li>
-        <li><Link to="/sign-in">ZAPISZ SIĘ</Link></li>
-      </ul>
-      <footer>Wszelkie prawa zastrzeżone</footer>
-    </div>
+    <React.Fragment>
+      <div
+        className={classes.backdrop}
+        ref={backdrop}
+        onClick={closeClickHandler}
+      ></div>
+      <div className={classes.overlay} ref={overlay}>
+        <i className="fa-solid fa-xmark" onClick={closeClickHandler}></i>
+        <ul>
+          <li>
+            <Link to="/" onClick={closeClickHandler}>
+              HOME
+            </Link>
+          </li>
+          <li>
+            <Link to="/wiki" onClick={closeClickHandler}>
+              WIKI
+            </Link>
+          </li>
+          <li>
+            <Link to="/sign-in" onClick={closeClickHandler}>
+              ZAPISZ SIĘ
+            </Link>
+          </li>
+        </ul>
+        <footer>Wszelkie prawa zastrzeżone</footer>
+      </div>
+    </React.Fragment>
   );
 
   return ReactDOM.createPortal(children, targetContainer);
