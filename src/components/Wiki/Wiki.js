c25c7c2f24f60908d137169340e1c0cb0caf83c3
Paweł <pawelporemba123@gmail.com>
2023-07-02 16:54:41 +0200

    Change wiki structure

--- src/components/Wiki/Wiki.js
+++ src/components/Wiki/Wiki.js
@@ -1,27 +1,8 @@
-import { useEffect, useContext } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
-
-import WidthContext from "../../store/width-context";
-import SearchInput from "../Search/SearchInput";
+import { Outlet } from "react-router-dom";
 
 const Wiki = () => {
-  const navigate = useNavigate();
-  const { width } = useContext(WidthContext);
-
-  useEffect(() => {
-    const footer = document.querySelector("footer");
-
-    footer.style.position = "static";
-    footer.style.bottom = "initial";
-  }, []);
-
-  const searchInputClickHandler = () => {
-    navigate("/szukaj");
-  };
-
   return (
     <main>
-      {width <= 480 && <SearchInput onClick={searchInputClickHandler} />}
       <Outlet />
     </main>
   );
