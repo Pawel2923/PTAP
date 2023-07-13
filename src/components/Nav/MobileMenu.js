26caed492a37b59fa37d064c546820edd7ac7cd2
Paweł <pawelporemba123@gmail.com>
2023-07-13 21:13:04 +0200

    Update navigation

--- src/components/Nav/MobileMenu.js
+++ src/components/Nav/MobileMenu.js
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
+import React, { useContext } from "react";
 import ReactDOM from "react-dom";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import classes from "./MobileMenu.module.css";
-
-const targetContainer = document.getElementById("overlays");
+import SearchContext from "../../store/search-context";
+import PageContext from "../../store/page-context";
 
 const verifyLink = (currentLocation, linkAddress) => {
   let highlightCondition = currentLocation === linkAddress;
@@ -18,62 +18,92 @@ const verifyLink = (currentLocation, linkAddress) => {
   return highlightCondition;
 };
 
-const linksList = [
+const menuList = [
   {
     name: "Główna",
     path: "/",
     icon: "icon-icon_house",
+    type: "link",
   },
   {
     name: "Szukaj",
     path: "/szukaj",
-    icon: "icon-icon_magnifying_glass"
+    icon: "icon-icon_magnifying_glass",
+    type: "button",
   },
   {
     name: "Wiki",
     path: "/wiki",
     icon: "icon-icon_globe",
+    type: "link",
   },
   {
     name: "Zapisz się",
     path: "/rejestracja",
     icon: "icon-icon_signup",
+    type: "link",
   },
 ];
 
 const MobileMenu = () => {
-  const location = useLocation();
-  const [currentLocation, setCurrentLocation] = useState("");
+  const { setIsShown } = useContext(SearchContext);
+  const { currentPage, setCurrentPage } = useContext(PageContext);
 
-  useEffect(() => {
-    setCurrentLocation(location.pathname);
-  }, [location]);
+  const linkClickHandler = (path) => {
+    setCurrentPage(path);
+    setIsShown(false);
+  };
+
+  const buttonClickHandler = () => {
+    setIsShown(true);
+    setCurrentPage("/szukaj");
+  };
 
   const nav = (
     <nav className={classes.menu}>
       <ul>
-        {linksList.map((link) => (
-          <li key={link.name}>
-            <NavLink
-              to={link.path}
-              className={
-                verifyLink(currentLocation, link.path) ? classes.highlight : ""
-              }
-            >
-              {verifyLink(currentLocation, link.path) ? (
-                <i className={`${link.icon}_solid`}></i>
-              ) : (
-                <i className={link.icon}></i>
-              )}
-              {link.name}
-            </NavLink>
+        {menuList.map((option) => (
+          <li key={option.name}>
+            {option.type === "link" ? (
+              <NavLink
+                to={option.path}
+                className={
+                  verifyLink(currentPage, option.path) ? classes.highlight : ""
+                }
+                onClick={() => {
+                  linkClickHandler(option.path);
+                }}
+              >
+                {verifyLink(currentPage, option.path) ? (
+                  <i className={`${option.icon}_solid`}></i>
+                ) : (
+                  <i className={option.icon}></i>
+                )}
+                {option.name}
+              </NavLink>
+            ) : (
+              <div
+                onClick={buttonClickHandler}
+                to={option.path}
+                className={
+                  verifyLink(currentPage, option.path) ? classes.highlight : ""
+                }
+              >
+                {verifyLink(currentPage, option.path) ? (
+                  <i className={`${option.icon}_solid`}></i>
+                ) : (
+                  <i className={option.icon}></i>
+                )}
+                {option.name}
+              </div>
+            )}
           </li>
         ))}
       </ul>
     </nav>
   );
 
-  return ReactDOM.createPortal(nav, targetContainer);
+  return ReactDOM.createPortal(nav, document.getElementById("overlays"));
 };
 
-export default MobileMenu;
+export default React.memo(MobileMenu);
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,43 +1,48 @@
 import React, { useContext } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
+import { SearchPlaceholder } from "../Search/Search";
 import MobileMenu from "./MobileMenu";
-import WidthContext from "../../store/width-context";
+import PageContext from "../../store/page-context";
 import classes from "./Nav.module.css";
 
 const Nav = () => {
-  const navigate = useNavigate();
-  const { width } = useContext(WidthContext);
+  const { width, setCurrentPage } = useContext(PageContext);
 
-  const logoClickHandler = () => {
-    navigate("/");
+  const linkClickHandler = (ev) => {
+    setCurrentPage(ev.currentTarget.getAttribute("href"));
   };
 
-  let imageTitle = width > 720 ? "Pilot Training Arma Project" : "PTAP";
+  let imageTitle = width >= 1000 ? "Pilot Training Arma Project" : "PTAP";
 
   let navContent = (
     <React.Fragment>
-      <div
-        style={{ cursor: "pointer" }}
-        title="Przejdź na stronę główną"
-        onClick={logoClickHandler}
-      >
+      <Link to="/" title="Przejdź na stronę główną" onClick={linkClickHandler}>
         <img
           src={require("../../img/logo.webp")}
           alt="logo"
           style={{ display: "block" }}
         />
         <h2>{imageTitle}</h2>
-      </div>
+      </Link>
       <ul>
         <li>
-          <NavLink to="/">Strona główna</NavLink>
+          <NavLink to="/" onClick={linkClickHandler}>
+            Strona główna
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/wiki">Wiki</NavLink>
+          <NavLink to="/wiki" onClick={linkClickHandler}>
+            Wiki
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/rejestracja">Zapisz się</NavLink>
+          <NavLink to="/rejestracja" onClick={linkClickHandler}>
+            Zapisz się
+          </NavLink>
+        </li>
+        <li>
+          <SearchPlaceholder />
         </li>
       </ul>
     </React.Fragment>
@@ -45,7 +50,7 @@ const Nav = () => {
 
   return (
     <React.Fragment>
-      {width > 480 ? (
+      {width > 740 ? (
         <nav className={classes.nav}>{navContent}</nav>
       ) : (
         <MobileMenu />
@@ -54,4 +59,4 @@ const Nav = () => {
   );
 };
 
-export default Nav;
+export default React.memo(Nav);
