465ea5030c1eed6e64cb0147917859038bb05ae8
Paweł <pawelporemba123@gmail.com>
2023-07-13 21:12:36 +0200

    Update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 
 import classes from "./Footer.module.css";
-import WidthContext from "../../store/width-context";
+import PageContext from "../../store/page-context";
 
 const Footer = () => {
   const [navHeight, setNavHeight] = useState(0);
-  const { width } = useContext(WidthContext);
+  const { width } = useContext(PageContext);
 
   useEffect(() => {
-    if (document.querySelector("nav") && width <= 480) {
+    if (document.querySelector("nav") && width <= 740) {
       setNavHeight(document.querySelector("nav").clientHeight);
     } else {
       setNavHeight(0);
@@ -17,24 +17,28 @@ const Footer = () => {
   }, [width]);
 
   return (
-    <React.Fragment>
-      <footer
-        className={classes.footer}
-        style={{ marginBottom: `${navHeight}px` }}
-      >
-        <div className={classes.top}>
-          <ul>
-            <li><Link to="/sign-up">Zapisz się do projektu</Link></li>
-            <li><Link to="/contact">Skontaktuj się z nami</Link></li>
-            <li><Link to ="/feedback">Zgłoś błąd na stronie</Link></li>
-          </ul>
-        </div>
-        <div className={classes.bottom}>
-          <p>PTAP 2018-2023 &copy; Wszelkie prawa zastrzeżone</p>
-        </div>
-      </footer>
-    </React.Fragment>
+    <footer
+      className={classes.footer}
+      style={{ marginBottom: `${navHeight}px` }}
+    >
+      <div className={classes.top}>
+        <ul>
+          <li>
+            <Link to="/sign-up">Zapisz się do projektu</Link>
+          </li>
+          <li>
+            <Link to="/contact">Skontaktuj się z nami</Link>
+          </li>
+          <li>
+            <Link to="/feedback">Zgłoś błąd na stronie</Link>
+          </li>
+        </ul>
+      </div>
+      <div className={classes.bottom}>
+        <p>PTAP 2018-2023 &copy; Wszelkie prawa zastrzeżone</p>
+      </div>
+    </footer>
   );
 };
 
-export default Footer;
+export default React.memo(Footer);
