a4cef92a969e46d9b36a8d91c76fc79fe6680e76
Paweł <pawelporemba123@gmail.com>
2023-07-29 12:29:17 +0200

    Add HeliSize.jsx

---
+++ src/components/Wiki/Articles/HeliSize.jsx
@@ -0,0 +1,21 @@
+const HeliSize = () => {
+  return (
+    <section className="section">
+      <header>
+        <h2>Helikoptery ze względu na rozmiar</h2>
+      </header>
+      <div className={classes.content}>
+        <h3>Lekkie śmigłowce</h3>
+        <p>
+          Śmigłowce lekkie służą do zwiadu lub transportu piechoty. Te
+          helikoptery mogą unosić niewielkich rozmiarów ładunek na zawiesiu.
+          Śmigłowce lekkie zwykle mają mniej pancerza, co nadrabiają
+          manewrowością i niewielkimi gabarytami. Śmigłowce lekkie są idealne
+          dla początkujących pilotów do nauki podstawowych manewrów.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default HeliSize;
