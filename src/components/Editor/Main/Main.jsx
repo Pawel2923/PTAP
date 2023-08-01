a20cb0645738d34e6b40e6bee004797c0810e929
Paweł <pawelporemba123@gmail.com>
2023-08-01 13:26:55 +0000

    Update console

---
+++ src/components/Editor/Main/Main.jsx
@@ -0,0 +1,16 @@
+import classes from "./Main.module.css";
+
+const Main = () => {
+    return (
+        <div className={classes.container}>
+            <header>
+                <h2>Przydatne narzędzia</h2>
+            </header>
+            <main className={classes.main}>
+                
+            </main>
+        </div>
+    );
+};
+
+export default Main;
--- src/components/Editor/Menu/Menu.jsx
+++ src/components/Editor/Menu/Menu.jsx
@@ -7,36 +7,40 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 const Menu = () => {
-  const navigate = useNavigate();
-  const { currentPage, setCurrentPage } = useContext(ConsoleContext);
+    const navigate = useNavigate();
+    const { currentPage, setCurrentPage } = useContext(ConsoleContext);
 
-  const optionClickHandler = (ev) => {
-    if (currentPage !== ev.currentTarget.id) {
-      setCurrentPage(ev.currentTarget.id);
-    }
-  };
+    const optionClickHandler = (ev) => {
+        if (currentPage !== ev.currentTarget.id) {
+            setCurrentPage(ev.currentTarget.id);
+        }
+    };
 
-  const exitClickHandler = () => {
-    setCurrentPage("home");
-    navigate("/");
-  };
+    const exitClickHandler = () => {
+        setCurrentPage("home");
+        navigate("/");
+    };
 
-  return (
-    <nav className={classes.menu}>
-      <Option id="home" onClick={optionClickHandler}>
-        <FontAwesomeIcon icon={icon({ name: "house", style: "solid" })} />
-        <span> Główna</span>
-      </Option>
-      <Option id="editor" onClick={optionClickHandler}>
-        <FontAwesomeIcon icon={icon({ name: "pencil", style: "solid" })} />
-        <span> Edytor</span>
-      </Option>
-      <Option id="exit" onClick={exitClickHandler}>
-        <FontAwesomeIcon icon={icon({ name: "right-from-bracket", style: "solid" })} />
-        <span> Wyjdź</span>
-      </Option>
-    </nav>
-  );
+    return (
+        <nav className={classes.menu}>
+            <Option id="home" onClick={optionClickHandler}>
+                <FontAwesomeIcon icon={icon({ name: "house", style: "solid" })} />
+                <span> Główna</span>
+            </Option>
+            <Option id="editor" onClick={optionClickHandler}>
+                <FontAwesomeIcon
+                    icon={icon({ name: "pencil", style: "solid" })}
+                />
+                <span> Edytor</span>
+            </Option>
+            <Option id="exit" onClick={exitClickHandler}>
+                <FontAwesomeIcon
+                    icon={icon({ name: "right-from-bracket", style: "solid" })}
+                />
+                <span> Wyjdź</span>
+            </Option>
+        </nav>
+    );
 };
 
 export default Menu;
