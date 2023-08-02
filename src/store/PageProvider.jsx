7dcd7d5e44fd1eb53b76cca2876845b1f65c37e7
Paweł <pawelporemba123@gmail.com>
2023-08-02 14:47:09 +0000

    Add fullscreen handling to pageProvider

--- src/store/PageProvider.jsx
+++ src/store/PageProvider.jsx
@@ -4,28 +4,75 @@ import PropTypes from "prop-types";
 import PageContext from "./page-context";
 
 const WidthProvider = ({ children }) => {
-  const [width, setWidth] = useState(window.innerWidth);
-  const [height, setHeight] = useState(window.innerHeight);
-  const [currentPage, setCurrentPage] = useState(document.location.pathname);
-  const value = { width, height, currentPage, setCurrentPage };
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+    const [currentPage, setCurrentPage] = useState(document.location.pathname);
+    const [fullscreen, setFullscreen] = useState(false);
 
-  useEffect(() => {
-    const handleWindowResize = () => {
-      setWidth(window.innerWidth);
-      setHeight(window.innerHeight);
+    const value = {
+        width,
+        height,
+        currentPage,
+        setCurrentPage,
+        fullscreen,
+        setFullscreen,
     };
-    window.addEventListener("resize", handleWindowResize);
 
-    return () => {
-      window.removeEventListener("resize", handleWindowResize);
-    };
-  }, []);
+    useEffect(() => {
+        const openFullscreen = () => {
+            const elem = document.documentElement;
 
-  return <PageContext.Provider value={value}>{children}</PageContext.Provider>;
+            if (elem.requestFullscreen) {
+                elem.requestFullscreen();
+            } else if (elem.webkitRequestFullscreen) {
+                /* Safari */
+                elem.webkitRequestFullscreen();
+            } else if (elem.msRequestFullscreen) {
+                /* IE11 */
+                elem.msRequestFullscreen();
+            }
+        };
+
+        const closeFullscreen = () => {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.webkitExitFullscreen) {
+                /* Safari */
+                document.webkitExitFullscreen();
+            } else if (document.msExitFullscreen) {
+                /* IE11 */
+                document.msExitFullscreen();
+            }
+        };
+
+        if (fullscreen) {
+            openFullscreen();
+        } else {
+            if (document.fullscreenElement) {
+                closeFullscreen();
+            }
+        }
+    }, [fullscreen]);
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWidth(window.innerWidth);
+            setHeight(window.innerHeight);
+        };
+        window.addEventListener("resize", handleWindowResize);
+
+        return () => {
+            window.removeEventListener("resize", handleWindowResize);
+        };
+    }, []);
+
+    return (
+        <PageContext.Provider value={value}>{children}</PageContext.Provider>
+    );
 };
 
 WidthProvider.propTypes = {
-  children: PropTypes.any
+    children: PropTypes.any,
 };
 
 export default WidthProvider;
--- src/store/page-context.js
+++ src/store/page-context.js
@@ -1,10 +1,12 @@
 import { createContext } from "react";
 
 const PageContext = createContext({
-  width: 0,
-  height: 0,
-  currentPage: "/",
-  setCurrentPage: () => {}
+    width: 0,
+    height: 0,
+    currentPage: "/",
+    setCurrentPage: () => {},
+    fullscreen: false,
+    setFullscreen: () => {},
 });
 
 export default PageContext;
