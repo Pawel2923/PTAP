94229fe5cd6d0a84452b824e12bfc2857c35c5a4
Paweł <pawelporemba123@gmail.com>
2023-08-02 10:43:49 +0000

    Update Main

--- src/components/Editor/Main/Main.jsx
+++ src/components/Editor/Main/Main.jsx
@@ -1,14 +1,28 @@
+import { useState, useContext, useEffect } from "react";
 import classes from "./Main.module.css";
+import ConsoleContext from "../../../store/console-context";
+import Home from "./Home";
+import Editor from "./Editor";
 
 const Main = () => {
+    const { currentPage } = useContext(ConsoleContext);
+    const [mainHeader, setMainHeader] = useState("");
+    const [pageContent, setPageContent] = useState("");
+
+    useEffect(() => {
+        if (currentPage === "home") {
+            setPageContent(<Home setMainHeader={setMainHeader} />);
+        } else if (currentPage === "editor") {
+            setPageContent(<Editor setMainHeader={setMainHeader} />);
+        }
+    }, [currentPage]);
+
     return (
         <div className={classes.container}>
             <header>
-                <h2>Przydatne narzędzia</h2>
+                <h2>{mainHeader}</h2>
             </header>
-            <main className={classes.main}>
-                
-            </main>
+            <main className={classes.main}>{pageContent}</main>
         </div>
     );
 };
