4628208f8213858a7193ed74abfd57f95c08231f
Paweł <pawelporemba123@gmail.com>
2023-08-09 19:10:30 +0000

    Add Edit

---
+++ src/components/Console/Main/Editor/Edit.jsx
@@ -0,0 +1,11 @@
+import classes from "./Edit.module.css";
+
+const Edit = () => {
+    return (
+        <div className={classes.edit}>
+            test
+        </div>
+    );
+};
+
+export default Edit;
