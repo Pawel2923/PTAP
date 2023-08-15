6ab7da8a45893df7c13d9a0fd13172a35fa684bb
Paweł Poremba <pawelporemba123@gamil.com>
2023-08-15 15:00:52 +0000

    Add Prism highlighting

--- src/components/Console/Main/Editor/Edit.jsx
+++ src/components/Console/Main/Editor/Edit.jsx
@@ -1,25 +1,41 @@
-import { useState } from "react";
-// import Prism from "prismjs";
+import { useRef, useState } from "react";
+import Prism from "prismjs";
+import "/src/css/prism.css";
 import classes from "./Edit.module.css";
 
 String.prototype.lines = function () { return this.split("\n"); }
 String.prototype.lineCount = function () { return this.lines().length; }
 
 const Edit = () => {
+    const codeRef = useRef(null);
     const [lineNumber, setLineNumber] = useState(1);
 
-    const textareaChangeHandler = (ev) => {
-        setLineNumber(ev.target.value.lineCount());
+    const textareaInputHandler = (ev) => {
+        let text = ev.target.value;
+        setLineNumber(text.lineCount());
+
+        if (text[text.length - 1] == "\n") { // If the last character is a newline character
+            text += " "; // Add a placeholder space character to the final line 
+        }
+
+        codeRef.current.innerHTML = Prism.highlight(text.replace(new RegExp("&", "g"), "&").replace(new RegExp("<", "g"), "<"), Prism.languages.html, "html");
+        
     };
 
     const textareakeyDownHandler = (ev) => {
         if (ev.key == "Tab") {
+            let code = ev.target.value;
+            const element = ev.target;
+
             ev.preventDefault()
 
-            const start = ev.target.selectionStart
-            const end = ev.target.selectionEnd
-
-            ev.target.value = ev.target.value.substring(0, start) + '\t' + ev.target.value.substring(end)
+            let before_tab = code.slice(0, element.selectionStart); // text before tab
+            let after_tab = code.slice(element.selectionEnd, element.value.length); // text after tab
+            let cursor_pos = element.selectionEnd + 1; // where cursor moves after tab - moving forward by 1 char to after tab
+            element.value = before_tab + "\t" + after_tab; // add tab char
+            // move cursor
+            element.selectionStart = cursor_pos;
+            element.selectionEnd = cursor_pos;
         }
     };
 
@@ -28,7 +44,12 @@ const Edit = () => {
             <div className={classes["line-numbers"]}>
                 {[...Array(lineNumber)].map((item, key) => <span key={key}></span>)}
             </div>
-            <textarea onChange={textareaChangeHandler} onKeyDown={textareakeyDownHandler} spellCheck={false}></textarea>
+            <div className={classes.content}>
+                <textarea onInput={textareaInputHandler} onKeyDown={textareakeyDownHandler} spellCheck={false}></textarea>
+                <pre>
+                    <code className="language-html" ref={codeRef}></code>
+                </pre>
+            </div>
         </div>
     );
 };
