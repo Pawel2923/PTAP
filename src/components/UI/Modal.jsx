8e6800cc733ecde88e7d9f304468e46401610dfa
Paweł <pawelporemba123@gmail.com>
2023-08-21 20:16:12 +0000

    Add Modal

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import classes from "./Modal.module.css";
+import icons from "../../icons/symbol-defs.svg";
+
+const Modal = ({ title, setShowModal, children }) => {
+    const closeClickHandler = () => {
+        setShowModal(false);
+    };
+
+    return (
+        <>
+            <div className={classes.backdrop} onClick={closeClickHandler}></div>
+            <div className={classes.modal}>
+                <h2>{title}</h2>
+                <p>{children}</p>
+                <div className={classes.close} onClick={closeClickHandler}>
+                    <svg className="icon icon-circle_xmark">
+                        <use xlinkHref={`${icons}#icon-circle_xmark`}></use>
+                    </svg>
+                </div>
+            </div>
+        </>
+    );
+};
+
+Modal.propTypes = {
+    title: PropTypes.string,
+    setShowModal: PropTypes.func,
+    children: PropTypes.any,
+};
+
+export default Modal;
