4162e81678abb359a215f1f47606978d6575135c
Paweł <pawelporemba123@gmail.com>
2023-08-21 20:16:46 +0000

    Modal test

--- src/components/Console/Console.jsx
+++ src/components/Console/Console.jsx
@@ -1,20 +1,25 @@
+import { useState } from "react";
 import ReactDOM from "react-dom";
 import classes from "./Console.module.css";
 import Menu from "./Menu/Menu";
 import ConsoleProvider from "../../store/ConsoleProvider";
 import Main from "./Main/Main";
+import Modal from "../UI/Modal";
 
 const Console = () => {
-  const overlay = (
-    <ConsoleProvider>
-      <div className={classes.overlay}>
-        <Menu />
-        <Main />
-      </div>
-    </ConsoleProvider>
-  );
+    const [showModal, setShowModal] = useState(true);
 
-  return ReactDOM.createPortal(overlay, document.getElementById("overlays"));
+    const overlay = (
+        <ConsoleProvider>
+            <div className={classes.overlay}>
+                <Menu />
+                <Main />
+            </div>
+            {showModal && <Modal title="Test" setShowModal={setShowModal}>Wiadomość</Modal>}
+        </ConsoleProvider>
+    );
+
+    return ReactDOM.createPortal(overlay, document.getElementById("overlays"));
 };
 
 export default Console;
