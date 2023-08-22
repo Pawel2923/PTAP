e21da8af26b381e3d007b101dfee90dbe886b613
Paweł <pawelporemba123@gmail.com>
2023-08-22 08:01:31 +0000

    Update button

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -3,37 +3,68 @@ import PropTypes from "prop-types";
 
 import classes from "./Button.module.css";
 
-export const ButtonLink = ({ to, children }) => {
-  return (
-    <Link to={to} className={classes["button-link"]}>
-      {children}
-    </Link>
-  );
+export const ButtonLink = ({ to, highlighted, onClick, children }) => {
+    if (highlighted == null) {
+        highlighted = true;
+    }
+
+    return (
+        <Link
+            to={to}
+            className={`${classes["button-link"]} ${
+                highlighted ? classes.highlight : ""
+            }`}
+            onClick={onClick ? onClick : () => {}}
+        >
+            {children}
+        </Link>
+    );
 };
 
 ButtonLink.propTypes = {
-  to: PropTypes.string.isRequired,
-  children: PropTypes.any
+    to: PropTypes.string.isRequired,
+    highlighted: PropTypes.bool,
+    onClick: PropTypes.func,
+    children: PropTypes.any,
 };
 
-export const Button = ({ to, type, children }) => {
-  const clickHandler = () => {
-    navigate(to);
-  };
+export const Button = ({
+    to,
+    className,
+    highlighted,
+    type,
+    onClick,
+    children,
+}) => {
+    const clickHandler = () => {
+        navigate(to);
+    };
 
-  return (
-    <button
-      type={type ? type : "button"}
-      onClick={to ? clickHandler : () => {}}
-      className={classes.button}
-    >
-      {children}
-    </button>
-  );
+    if (highlighted == null) {
+        highlighted = true;
+    }
+
+    return (
+        <button
+            type={type ? type : "button"}
+            onClick={() => {
+                to && clickHandler();
+                onClick && onClick();
+            }}
+            className={`${classes.button} ${className ? className : ""} ${
+                highlighted ? classes.highlight : ""
+            }`}
+        >
+            {children}
+        </button>
+    );
 };
 
 Button.propTypes = {
-  to: PropTypes.string,
-  type: PropTypes.string,
-  children: PropTypes.any
+    to: PropTypes.string,
+    className: PropTypes.string,
+    highlighted: PropTypes.bool,
+    type: PropTypes.string,
+    onClick: PropTypes.func,
+    children: PropTypes.any,
 };
