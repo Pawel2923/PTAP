276ca60401ae83d18b2ec74536c111563c9a8a56
Paweł <pawelporemba123@gmail.com>
2023-08-22 08:55:41 +0000

    Add toolbarButton to context

--- src/store/ConsoleProvider.jsx
+++ src/store/ConsoleProvider.jsx
@@ -1,29 +1,59 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import PropTypes from "prop-types";
 
 import ConsoleContext from "./console-context";
 
-const ConsoleProvider = ({ children }) => {
-  const defaultPage = sessionStorage.getItem("currentPage") ? sessionStorage.getItem("currentPage") : "home";
-  const [currentPage, setCurrentPage] = useState(defaultPage);
-  const [editorContent, setEditorContent] = useState("home");
-  const [articleCode, setArticleCode] = useState("");
-  const value = {
-    currentPage,
-    editorContent,
-    articleCode,
-    setCurrentPage,
-    setEditorContent,
-    setArticleCode,
-  };
+const defaultToolbarButtons = {
+    file: {
+        main: false,
+        exit: true,
+        new: false,
+        open: false,
+        save: false,
+        import: false,
+        export: false,
+    },
+};
 
-  return (
-    <ConsoleContext.Provider value={value}>{children}</ConsoleContext.Provider>
-  );
+const toolbarButtonsReducer = (state, action) => {
+    let newButtons = state;
+    if (action.newState !== null && action.mainButton && action.subButton) {
+        newButtons[action.mainButton][action.subButton] = action.newState;
+    }
+    return newButtons;
+};
+
+const ConsoleProvider = ({ children }) => {
+    const defaultPage = sessionStorage.getItem("currentPage")
+        ? sessionStorage.getItem("currentPage")
+        : "home";
+    const [currentPage, setCurrentPage] = useState(defaultPage);
+    const [editorContent, setEditorContent] = useState("home");
+    const [articleCode, setArticleCode] = useState("");
+    const [toolbarButtons, dispatchToolbarButtons] = useReducer(
+        toolbarButtonsReducer,
+        defaultToolbarButtons
+    );
+    const value = {
+        currentPage,
+        editorContent,
+        articleCode,
+        toolbarButtons,
+        setCurrentPage,
+        setEditorContent,
+        setArticleCode,
+        dispatchToolbarButtons,
+    };
+
+    return (
+        <ConsoleContext.Provider value={value}>
+            {children}
+        </ConsoleContext.Provider>
+    );
 };
 
 ConsoleProvider.propTypes = {
-  children: PropTypes.any,
+    children: PropTypes.any,
 };
 
 export default ConsoleProvider;
--- src/store/console-context.js
+++ src/store/console-context.js
@@ -1,12 +1,31 @@
 import { createContext } from "react";
 
 const ConsoleContext = createContext({
-  currentPage: "home",
-  editorContent: "home",
-  articleCode: "",
-  setCurrentPage: () => {},
-  setEditorContent: () => {},
-  setArticleCode: () => {},
+    currentPage: "home",
+    editorContent: "home",
+    articleCode: "",
+    toolbarButtons: {
+        file: {
+            main: false,
+            exit: true,
+            new: false,
+            open: false,
+            save: false,
+            import: false,
+            export: false,
+        },
+    },
+    setCurrentPage: () => {},
+    setEditorContent: () => {},
+    setArticleCode: () => {},
+    dispatchToolbarButtons: (
+        state,
+        action = {
+            mainButton: "Button name",
+            subButton: "Button name",
+            newState: true,
+        }
+    ) => {action},
 });
 
 export default ConsoleContext;
