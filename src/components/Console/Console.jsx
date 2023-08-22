42be95513794445994bc54ad37b7781207df8b27
Paweł <pawelporemba123@gmail.com>
2023-08-22 05:58:01 +0000

    Remove Modal test

--- src/components/Console/Console.jsx
+++ src/components/Console/Console.jsx
@@ -1,21 +1,16 @@
-import { useState } from "react";
 import ReactDOM from "react-dom";
 import classes from "./Console.module.css";
 import Menu from "./Menu/Menu";
 import ConsoleProvider from "../../store/ConsoleProvider";
 import Main from "./Main/Main";
-import Modal from "../UI/Modal";
 
 const Console = () => {
-    const [showModal, setShowModal] = useState(true);
-
     const overlay = (
         <ConsoleProvider>
             <div className={classes.overlay}>
                 <Menu />
                 <Main />
             </div>
-            {showModal && <Modal title="Test" setShowModal={setShowModal}>Wiadomość</Modal>}
         </ConsoleProvider>
     );
 
