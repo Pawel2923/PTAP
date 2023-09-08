4dfca77026eb5f258265d9ac926518b2addebfe8
Pawel <pawelporemba123@gmail.com>
2023-09-08 09:18:58 +0200

    Add form to Signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,13 +1,167 @@
+import React, { useState } from "react";
 import Footer from "../components/Footer/Footer";
+import Input from "../components/UI/Input";
+import Modal from "../components/UI/Modal";
+import { Button } from "../components/UI/Button";
+
+const defaultModalState = {
+	show: false,
+	error: false,
+	title: "",
+	message: "",
+};
 
 const Signup = () => {
-    return (
-        <>
-            <main>
-            </main>
-            <Footer />
-        </>
-    );
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [isInputValid, setIsInputValid] = useState(new Array.fill(4, false));
+	const [modalState, setModalState] = useState(defaultModalState);
+
+	const inputChangeHandler = (ev, inputId) => {
+		if (inputId === "name") {
+			setName(ev.target.value);
+		} else if (inputId === "email") {
+			setEmail(ev.target.value);
+		} else if (inputId === "password") {
+			setPassword(ev.target.value);
+		} else if (inputId === "confirmPassword") {
+			setConfirmPassword(ev.target.value);
+		}
+	};
+
+	const submitHandler = (ev) => {
+		ev.preventDefault();
+		let isValid = false;
+
+		const falseIndex = isInputValid.findIndex((val) => val === false);
+		if (falseIndex === -1) {
+			isValid = true;
+		}
+
+		if (isValid) {
+		} else {
+			setModalState({
+				show: true,
+				error: true,
+				title: `Wypełnij formularz`,
+				message:
+					"Sprawdź czy poprawnie wypełniłeś wszystkie pola formularza.",
+			});
+			return;
+		}
+
+		setName("");
+		setEmail("");
+		setPassword("");
+		setConfirmPassword("");
+		setIsInputValid(new Array(4).fill(false));
+	};
+
+	const modalCloseHandler = () => {
+		setModalState(defaultModalState);
+	};
+
+	const inputValidateHandler = (isValid, index) => {
+		if (index !== -1) {
+			setIsInputValid((prevState) => {
+				let updatedState = prevState;
+				updatedState[index] = isValid;
+				return updatedState;
+			});
+			return;
+		}
+		setIsInputValid(new Array(4).fill(false));
+	};
+
+	return (
+		<>
+			<main>
+				<section className={classes.signup}>
+					<h1>Zarejestruj się</h1>
+					<h2>
+						<span>Wypełnij formularz rejestracyjny</span>
+					</h2>
+					<form onSubmit={submitHandler}>
+						<label className={classes.name}>
+							<p>
+								Nazwa{" "}
+								<span className={classes.asterisk}>*</span>
+							</p>
+							<Input
+								attributes={{
+									type: "text",
+									id: "name",
+									value: name,
+									minLength: "3",
+									required: true,
+								}}
+								onChange={inputChangeHandler}
+								onValidate={inputValidateHandler}
+							/>
+						</label>
+						<label className={classes.email}>
+							<p>
+								E-mail{" "}
+								<span className={classes.asterisk}>*</span>
+							</p>
+							<Input
+								attributes={{
+									type: "email",
+									id: "email",
+									value: email,
+									required: true,
+								}}
+								onChange={inputChangeHandler}
+								onValidate={inputValidateHandler}
+							/>
+						</label>
+						<label className={classes.name}>
+							<p>
+								Hasło{" "}
+								<span className={classes.asterisk}>*</span>
+							</p>
+							<Input
+								attributes={{
+									type: "password",
+									id: "password",
+									value: password,
+									minLength: "3",
+									required: true,
+								}}
+								onChange={inputChangeHandler}
+								onValidate={inputValidateHandler}
+							/>
+						</label>
+						<label className={classes["confirm-password"]}>
+							<p>
+								Powtórz Hasło{" "}
+								<span className={classes.asterisk}>*</span>
+							</p>
+							<Input
+								attributes={{
+									type: "password",
+									id: "confirmPassword",
+									value: confirmPassword,
+									minLength: "3",
+									required: true,
+								}}
+								onChange={inputChangeHandler}
+								onValidate={inputValidateHandler}
+							/>
+						</label>
+						<Button type="submit">Wyślij</Button>
+					</form>
+				</section>
+				{isLoading && <Loading />}
+				{modalState.show && (
+					<Modal modalInfo={modalState} onClose={modalCloseHandler} />
+				)}
+			</main>
+			<Footer />
+		</>
+	);
 };
 
 export default Signup;
