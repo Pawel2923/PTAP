6caf270a063adf0f1152795f0eaf4f89f56ac2be
Paweł <pawelporemba123@gmail.com>
2023-09-12 15:36:37 +0200

    Add check for uid

--- src/pages/Console.jsx
+++ src/pages/Console.jsx
@@ -1,34 +1,63 @@
 import { useState, useContext, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import Menu from "../components/Console/Menu/Menu";
 import ConsoleProvider from "../store/ConsoleProvider";
 import Main from "../components/Console/Main/Main";
 import PageContext from "../store/page-context";
 import LoadingScreen from "../components/LoadingScreen";
+import useAuth from "../hooks/use-auth";
 import classes from "./Console.module.css";
 
 const Console = () => {
-  const menuRef = useRef(null);
-  const { width } = useContext(PageContext);
-  const [spacerStyles, setSpacerStyles] = useState({});
-  const [isMenuExpanded, setIsMenuExpanded] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+	const { uid } = useAuth();
+	const navigate = useNavigate();
+	const menuRef = useRef(null);
+	const { width } = useContext(PageContext);
+	const [spacerStyles, setSpacerStyles] = useState({});
+	const [isMenuExpanded, setIsMenuExpanded] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  console.log(uid);
 
   useEffect(() => {
-    const menuWidth = menuRef.current.clientWidth;
-    if (width <= 740 && !isMenuExpanded) {
-      setSpacerStyles({ width: menuWidth, height: "100vh" });
+    if (uid === null) {
+      setIsLoggedIn(false);
+      return;
+    } 
+    
+    setIsLoggedIn(true);
+  }, [uid]);
+
+	useEffect(() => {
+		if (isLoggedIn) {
+      const menuWidth = menuRef.current.clientWidth;
+      if (width <= 740 && !isMenuExpanded) {
+        setSpacerStyles({ width: menuWidth, height: "100vh" });
+      }
     }
-  }, [width, isMenuExpanded]);
+	}, [width, isMenuExpanded, isLoggedIn]);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate("/rejestracja", { replace: true });
+    }
+  }, [isLoggedIn, navigate]);
 
   return (
-    <ConsoleProvider>
-      {isLoading && <LoadingScreen isLoading={isLoading} setIsLoading={setIsLoading} />}
+    isLoggedIn && <ConsoleProvider>
+      {isLoading && (
+        <LoadingScreen
+          isLoading={isLoading}
+          setIsLoading={setIsLoading}
+        />
+      )}
       <div
-        className={`${!isMenuExpanded ? classes["shrink-menu"] : ""} ${
-          classes.console
-        }`}
+        className={`${
+          !isMenuExpanded ? classes["shrink-menu"] : ""
+        } ${classes.console}`}
       >
-        {(width <= 740) && <div style={spacerStyles}></div>}
+        {width <= 740 && <div style={spacerStyles}></div>}
         <Menu ref={menuRef} setIsMenuExpanded={setIsMenuExpanded} />
         <Main />
       </div>
